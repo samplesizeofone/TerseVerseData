ae855ce8e508f35d77ba2e665188692bba91bd44
Tyler Spaeth <samplesizeofone@gmail.com>
2015-01-17 08:18:04 -0800

    Add Makefile to build derived data.

--- cmu.py
+++ cmu.py
@@ -4,7 +4,7 @@ import nltk
 
 entries = nltk.corpus.cmudict.entries()
 
-with open('rhymes_out.csv', 'wb') as rhymes_file:
+with open('derived_data/cmu.csv', 'wb') as rhymes_file:
     rhymes_csv = csv.writer(rhymes_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
     for (word, phonemes) in entries:
         rhymes_csv.writerow([word] + phonemes)
